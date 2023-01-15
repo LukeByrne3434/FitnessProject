c67213f9849df47b90889e551a4a7745ef61071e
LukeByrne3434 <38034720+LukeByrne3434@users.noreply.github.com>
2023-01-15 14:05:12 +0000

    First Commit

--- angular-fitness-project/src/app/app.component.ts
+++ angular-fitness-project/src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import usersData from './users.json';
+
+interface User{
+  id:Number;
+  name:String;
+  email:String;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-fitness-project';
+
+  users:User[]=usersData;
 }
